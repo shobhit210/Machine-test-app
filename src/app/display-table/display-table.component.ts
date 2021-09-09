c5584e690be6e5c3595264dd6962f5eb69dae947
Shobhit Nair <shobhit.210@gmail.com>
2021-09-09 19:20:56 +0530

    final commit

--- src/app/display-table/display-table.component.ts
+++ src/app/display-table/display-table.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayTableComponent implements OnInit {
 
+  populateusers(users:any){
+    let api_data = "";
+        for(let user of users){
+            api_data += `<tr><td>${user.id}</td><td>${user.login}</td><td>${user.type}</td><td>${user.site_admin}</td><td>${user.node_id}</td></tr>`;
+        }
+        
+        let one =  document.getElementById("result")
+        let tr = document.createElement("tr");
+        
+      
+        tr.innerHTML = api_data
+        one?.appendChild(tr);
+
+        // result.innerHTML = html_data
+    
+  }
+  
+  fetchdetails(){
+    fetch('https://api.github.com/users').then(resp => resp.json()).then(data => this.populateusers(data));
+  }
+
   constructor() { }
 
   ngOnInit(): void {
